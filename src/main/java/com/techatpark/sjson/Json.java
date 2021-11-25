1473da1fae933bcb8b74364e460f2042b7ce592b
haripriya <rm.haripriya@gmail.com>
2021-11-25 22:59:27 +0530

    Shift Reader

--- src/main/java/com/techatpark/sjson/Json.java
+++ src/main/java/com/techatpark/sjson/Json.java
@@ -13,8 +13,8 @@ import java.util.Map;
 public final class Json {
 
     public Object read(final Reader reader) throws IOException {
-        try (reader) {
-            return getValue(reader);
+        try (Reader shiftReader = new ShiftReader(reader)) {
+            return getValue(shiftReader);
         }
     }
 
@@ -110,22 +110,20 @@ public final class Json {
         builder.append(startingChar);
 
         char character;
-        reader.mark(1);
+
         while ( Character.isDigit(character = (char) reader.read()) ) {
             builder.append(character);
-            reader.mark(1);
         }
 
         if(character == '.') {
             builder.append('.');
             while ( Character.isDigit(character = (char) reader.read()) ) {
                 builder.append(character);
-                reader.mark(1);
             }
-            reader.reset();
+            ((ShiftReader)reader).reverse(character);
             return Double.parseDouble(builder.toString());
         } else {
-            reader.reset();
+            ((ShiftReader)reader).reverse(character);
             return Long.parseLong(builder.toString());
         }
     }
@@ -146,7 +144,6 @@ public final class Json {
             default : return getNumber(reader,character);
         }
     }
-
 
     private boolean getTrue(final Reader reader) throws IOException {
         if ( (char) reader.read() == 'r'
@@ -194,5 +191,81 @@ public final class Json {
                 (char) reader.read(), (char) reader.read()});
     }
 
+    private class ShiftReader extends Reader {
 
+        private final Reader reader;
+
+        private int previous;
+
+        private ShiftReader(final Reader reader) {
+            this.reader = reader;
+            this.previous = 0;
+        }
+
+        void reverse(final int previous) {
+            this.previous = previous;
+        }
+
+        /**
+         * Reads a single character.  This method will block until a character is
+         * available, an I/O error occurs, or the end of the stream is reached.
+         *
+         * <p> Subclasses that intend to support efficient single-character input
+         * should override this method.
+         *
+         * @return The character read, as an integer in the range 0 to 65535
+         * ({@code 0x00-0xffff}), or -1 if the end of the stream has
+         * been reached
+         * @throws IOException If an I/O error occurs
+         */
+        @Override
+        public int read() throws IOException {
+            if(this.previous == 0) {
+                return this.reader.read();
+            }
+            else {
+                int temp = this.previous;
+                this.previous = 0;
+                return temp;
+            }
+        }
+
+        /**
+         * Reads characters into a portion of an array.  This method will block
+         * until some input is available, an I/O error occurs, or the end of the
+         * stream is reached.
+         *
+         * <p> If {@code len} is zero, then no characters are read and {@code 0} is
+         * returned; otherwise, there is an attempt to read at least one character.
+         * If no character is available because the stream is at its end, the value
+         * {@code -1} is returned; otherwise, at least one character is read and
+         * stored into {@code cbuf}.
+         *
+         * @param cbuf Destination buffer
+         * @param off  Offset at which to start storing characters
+         * @param len  Maximum number of characters to read
+         * @return The number of characters read, or -1 if the end of the
+         * stream has been reached
+         * @throws IndexOutOfBoundsException If {@code off} is negative, or {@code len} is negative,
+         *                                   or {@code len} is greater than {@code cbuf.length - off}
+         * @throws IOException               If an I/O error occurs
+         */
+        @Override
+        public int read(final char[] cbuf, final int off, final int len) throws IOException {
+            return this.reader.read(cbuf,off,len);
+        }
+
+        /**
+         * Closes the stream and releases any system resources associated with
+         * it.  Once the stream has been closed, further read(), ready(),
+         * mark(), reset(), or skip() invocations will throw an IOException.
+         * Closing a previously closed stream has no effect.
+         *
+         * @throws IOException If an I/O error occurs
+         */
+        @Override
+        public void close() throws IOException {
+            this.reader.close();
+        }
+    }
 }
