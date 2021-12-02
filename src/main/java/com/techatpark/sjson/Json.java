ee81f01191660b13eeda709e0d6fc3b1f85454ea
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-12-02 19:48:24 +0530

    Improve Number Calcl

--- src/main/java/com/techatpark/sjson/Json.java
+++ src/main/java/com/techatpark/sjson/Json.java
@@ -330,7 +330,30 @@ public final class Json {
         }
 
         private int getNumericValue(final char character) {
-            return Character.getNumericValue(character);
+            switch (character) {
+                case '0':
+                    return 0;
+                case '1':
+                    return 1;
+                case '2':
+                    return 2;
+                case '3':
+                    return 3;
+                case '4':
+                    return 4;
+                case '5':
+                    return 5;
+                case '6':
+                    return 6;
+                case '7':
+                    return 7;
+                case '8':
+                    return 8;
+                case '9':
+                    return 9;
+                default:
+                    throw new IllegalArgumentException("Inavlid JSON at");
+            }
         }
 
         private long getNumericValue(final char character, final int placement) {
