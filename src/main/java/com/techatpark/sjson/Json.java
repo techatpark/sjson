540b1f65595e1d15d97aa557a1c490aa63197aff
haripriya <rm.haripriya@gmail.com>
2022-04-14 10:42:43 +0530

    Fixed Test Cases

--- src/main/java/com/techatpark/sjson/Json.java
+++ src/main/java/com/techatpark/sjson/Json.java
@@ -258,7 +258,7 @@ public final class Json {
             }
             // Create Key enclosed with "
             builder.append("\"");
-            builder.append(entry.getKey());
+            builder.append(escapeJsonTxt(entry.getKey()));
 
             // Create Key value separator
             builder.append("\":");
@@ -322,10 +322,77 @@ public final class Json {
     private void processString(final StringBuilder builder,
                                final String value) {
         builder.append("\"")
-                .append(value)
+                .append(escapeJsonTxt(value))
                 .append("\"");
     }
 
+    /**
+     * Escape JSON Text.
+     * Escape quotes, \, /, \r, \n, \b, \f, \t
+     * and other control characters (U+0000 through U+001F).
+     * @param s
+     * @return escapeJsonTxt
+     */
+    private String escapeJsonTxt(final String s) {
+        if (s == null) {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+        escape(s, sb);
+        return sb.toString();
+    }
+
+    /**
+     * Escape Text.
+     * @param s - Must not be null.
+     * @param sb
+     */
+    private void escape(final String s, final StringBuffer sb) {
+        final int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
+                case '"':
+                    sb.append("\\\"");
+                    break;
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                case '/':
+                    sb.append("\\/");
+                    break;
+                default:
+                    //Reference: http://www.unicode.org/versions/Unicode5.1.0/
+                    if ((ch >= '\u0000' && ch <= '\u001F')
+                            || (ch >= '\u007F' && ch <= '\u009F')
+                            || (ch >= '\u2000' && ch <= '\u20FF')) {
+                        String ss = Integer.toHexString(ch);
+                        sb.append("\\u");
+                        for (int k = 0; k < NUMBER_FOUR - ss.length(); k++) {
+                            sb.append('0');
+                        }
+                        sb.append(ss.toUpperCase());
+                    } else {
+                        sb.append(ch);
+                    }
+            }
+        }
+    }
     /**
      * Process Number Array.
      *
