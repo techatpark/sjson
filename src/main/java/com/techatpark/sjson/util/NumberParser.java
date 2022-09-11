9c91259efafabf07434c146ef3c2a280de2dc65c
sen091 <sen091@gmail.com>
2022-09-11 20:28:01 +0530

    Added Number Parser classes

---
+++ src/main/java/com/techatpark/sjson/util/NumberParser.java
@@ -0,0 +1,223 @@
+package com.techatpark.sjson.util;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class NumberParser {
+
+    /**
+     * Number 1.
+     */
+    public static final int NUMBER_ONE = 1;
+    /**
+     * Number 2.
+     */
+    public static final int NUMBER_TWO = 2;
+    /**
+     * Number 3.
+     */
+    public static final int NUMBER_THREE = 3;
+    /**
+     * Number 4.
+     */
+    public static final int NUMBER_FOUR = 4;
+    /**
+     * Number 5.
+     */
+    public static final int NUMBER_FIVE = 5;
+    /**
+     * Number 6.
+     */
+    public static final int NUMBER_SIX = 6;
+    /**
+     * Number 7.
+     */
+    public static final int NUMBER_SEVEN = 7;
+    /**
+     * Number 8.
+     */
+    public static final int NUMBER_EIGHT = 8;
+    /**
+     * Number 9.
+     */
+    public static final int NUMBER_NINE = 9;
+    /**
+     * Number 10.
+     */
+    public static final int NUMBER_TEN = 10;
+    /**
+     * Number 11.
+     */
+    public static final int NUMBER_ELEVEN = 11;
+    /**
+     * Number 12.
+     */
+    public static final int NUMBER_TWELVE = 12;
+    /**
+     * Number 13.
+     */
+    public static final int NUMBER_THIRTEEN = 13;
+    /**
+     * Number 14.
+     */
+    public static final int NUMBER_FOURTEEN = 14;
+    /**
+     * Number 15.
+     */
+    public static final int NUMBER_FIFTEEN = 15;
+    /**
+     * Number 16.
+     */
+    public static final int NUMBER_SIXTEEN = 16;
+    /**
+     * Number 17.
+     */
+    public static final int NUMBER_SEVENTEEN = 17;
+    /**
+     * Number 18.
+     */
+    public static final int NUMBER_EIGHTEEN = 18;
+    /**
+     * Number 19.
+     */
+    public static final int NUMBER_NINETEEN = 19;
+
+    /**
+     * Gets Compact Number of a source String.
+     * @param source
+     * @param isNegative
+     * @return Byte, Short, Integer, Long or BigInteger
+     */
+    public Number parseNumber(final String source, final boolean isNegative) {
+        switch (source.length()) {
+            case NUMBER_ONE:
+            case NUMBER_TWO:
+                return isNegative ? (byte) -Byte.parseByte(source)
+                             : Byte.parseByte(source);
+            case NUMBER_THREE:
+                return getByteOrShort(source, isNegative);
+            case NUMBER_FOUR:
+                return isNegative ? (short) -Short.parseShort(source)
+                        : Short.parseShort(source);
+            case NUMBER_FIVE:
+                return getShortOrInteger(source, isNegative);
+            case NUMBER_SIX:
+            case NUMBER_SEVEN:
+            case NUMBER_EIGHT:
+            case NUMBER_NINE:
+                return isNegative ? -Integer.parseUnsignedInt(source)
+                        : Integer.parseUnsignedInt(source);
+            case NUMBER_TEN:
+                return getIntegerOrLong(source, isNegative);
+            case NUMBER_ELEVEN:
+            case NUMBER_TWELVE:
+            case NUMBER_THIRTEEN:
+            case NUMBER_FOURTEEN:
+            case NUMBER_FIFTEEN:
+            case NUMBER_SIXTEEN:
+            case NUMBER_SEVENTEEN:
+            case NUMBER_EIGHTEEN:
+                return isNegative ? -Long.parseUnsignedLong(source)
+                        : Long.parseUnsignedLong(source);
+            case NUMBER_NINETEEN:
+                return getLongOrBigNumber(source, isNegative);
+            default:
+                return new BigInteger(source);
+        }
+    }
+
+
+
+    private Number getByteOrShort(final String source,
+                                  final boolean isNegative) {
+        short aShort = Short.parseShort(source);
+        if (isNegative) {
+            aShort = (short) -aShort;
+            if (aShort >= Byte.MIN_VALUE) {
+                return (byte) aShort;
+            }
+            return aShort;
+
+        } else {
+            if (aShort <= Byte.MAX_VALUE) {
+                return (byte) aShort;
+            }
+            return aShort;
+        }
+
+
+    }
+
+    private Number getShortOrInteger(final String source,
+                                     final boolean isNegative) {
+        int integer = Integer.parseUnsignedInt(source);
+        if (isNegative) {
+            integer = -integer;
+            if (integer >= Short.MIN_VALUE) {
+                return (short) integer;
+            }
+            return integer;
+        } else {
+            if (integer <= Short.MAX_VALUE) {
+                return (short) integer;
+            }
+            return integer;
+        }
+
+    }
+
+    private Number getIntegerOrLong(final String source,
+                                    final boolean isNegative) {
+        long aLong = Long.parseUnsignedLong(source);
+        if (isNegative) {
+            aLong = -aLong;
+            if (aLong >= Integer.MIN_VALUE) {
+                return (int) aLong;
+            }
+            return aLong;
+        } else {
+            if (aLong <= Integer.MAX_VALUE) {
+                return (int) aLong;
+            }
+            return aLong;
+        }
+
+    }
+
+    private Number getLongOrBigNumber(final String source,
+                                      final boolean isNegative) {
+        BigInteger bigInteger = new BigInteger(source);
+        if (isNegative) {
+            bigInteger = bigInteger.multiply(new BigInteger("-1"));
+            if (bigInteger.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) < -1) {
+                return bigInteger.longValue();
+            }
+            return bigInteger;
+
+        } else {
+            if (bigInteger.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > -1) {
+                return bigInteger.longValue();
+            }
+            return bigInteger;
+        }
+    }
+
+    /**
+     * Gets Compact decimal Number of a source String.
+     * @param source
+     * @return Float,Double or BigDecimal
+     */
+    public Number parseDecimalNumber(final String source) {
+        float aFloat = Float.parseFloat(source);
+        if (aFloat == Float.POSITIVE_INFINITY) {
+            double aDouble = Double.parseDouble(source);
+            if (aDouble == Double.POSITIVE_INFINITY) {
+                return new BigDecimal(source);
+            }
+            return aDouble;
+        }
+
+        return aFloat;
+    }
+
+}
---
+++ src/test/java/com/techatpark/sjson/util/NumberParserTest.java
@@ -0,0 +1,114 @@
+package com.techatpark.sjson.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberParserTest {
+
+    @Test
+    void parserNumber()  {
+
+        NumberParser parser = new NumberParser();
+
+        Byte aByte;
+
+        Assertions.assertEquals(
+                (byte) -99,
+                parser.parseNumber(String.valueOf(99),true),
+                "Byte identified properly for ");
+
+        Assertions.assertEquals(
+                (byte) 99,
+                parser.parseNumber(String.valueOf(99),false),
+                "Byte identified properly for ");
+
+        for (int i = Byte.MIN_VALUE; i < 0; i++) {
+            aByte = (Byte) parser.parseNumber(String.valueOf(-i),true);
+            Assertions.assertEquals(
+                    (byte) i,
+                    aByte,
+                    "Byte identified properly for " + i);
+        }
+
+        for (int i = 126; i <= Byte.MAX_VALUE; i++) {
+            aByte = (Byte) parser.parseNumber(String.valueOf(i),false);
+            Assertions.assertEquals(
+                    (byte) i,
+                    aByte,
+                    "Byte  identified properly for " + i);
+        }
+
+        Short aShort ;
+
+        for (int i = Short.MIN_VALUE; i < -10000; i++) {
+            aShort = (Short) parser.parseNumber(String.valueOf(-i),true);
+            Assertions.assertEquals(
+                    (short) i,
+                    aShort,
+                    "Short  not identified properly for " + i);
+        }
+
+        for (int i = 9999; i <= Short.MAX_VALUE; i++) {
+            Assertions.assertInstanceOf(
+                    Short.class,
+                    parser.parseNumber(String.valueOf(i),false),
+                    "Short  not identified properly for " + i);
+        }
+
+        Integer aInteger;
+
+        for (int i = -1000000000; i < -999999998; i++) {
+            aInteger = (Integer) parser.parseNumber(String.valueOf( ((long) i )* -1),true);
+            Assertions.assertEquals(
+                    i,
+                    aInteger,
+                    "Integer  not identified properly for " + i);
+        }
+
+        for (int i = 2147483640; i >= Integer.MAX_VALUE; ++i) {
+            Assertions.assertInstanceOf(
+                    Integer.class,
+                    parser.parseNumber(Integer.toString(i),false),
+                    "Integer not identified properly for " + i);
+        }
+
+        Assertions.assertInstanceOf(
+                Long.class,
+                parser.parseNumber(Long.toString(Long.MAX_VALUE),false),
+                "Long Max not identified properly");
+
+        Assertions.assertInstanceOf(
+                BigInteger.class,
+                parser.parseNumber(BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.TWO).toString(),false),
+                "BigInteger Max not identified properly");
+    }
+
+    @Test
+    void parserDecimalNumber() throws ParseException {
+
+        NumberParser parser = new NumberParser();
+
+        Assertions.assertInstanceOf(
+                Float.class,
+                parser.parseDecimalNumber(Float.toString(Float.MAX_VALUE)),
+                "Float Max not identified properly");
+
+        Assertions.assertInstanceOf(
+                Double.class,
+                parser.parseDecimalNumber(Double.toString(Double.MAX_VALUE)),
+                "Double Max not identified properly");
+
+        Assertions.assertInstanceOf(
+                BigDecimal.class,
+                parser.parseDecimalNumber(BigDecimal.valueOf(Double.MAX_VALUE)
+                        .multiply(BigDecimal.TEN).toString()),
+                "Double Max not identified properly");
+
+    }
+}
