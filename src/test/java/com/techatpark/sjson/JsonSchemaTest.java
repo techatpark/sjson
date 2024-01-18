8dee452e904b94ba17e0b743830db659c4f714b5
Mohit.Sahu <mohitsarva11@gmail.com>
2024-01-18 20:33:19 +0530

    build error solved

--- src/test/java/com/techatpark/sjson/JsonSchemaTest.java
+++ src/test/java/com/techatpark/sjson/JsonSchemaTest.java
@@ -1,22 +1,76 @@
 package com.techatpark.sjson;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-class JsonSchemaTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JsonSchemaTest {
+
+    private JsonSchema jsonSchema;
+
+    @BeforeEach
+    public void setUp() {
+        jsonSchema = new JsonSchema();
+    }
 
     @Test
-    void read() throws IOException {
+    public void testJsonTextWithSimpleMap() {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("key1", "value1");
+        jsonMap.put("key2", 123);
 
-        JsonSchema jsonSchema =
-                new JsonSchema(new FileReader("src/test/resources/schemas/product.json"));
+        String expectedJson = "{\"key1\":\"value1\",\"key2\":123}";
+        assertEquals(expectedJson, jsonSchema.jsonText(jsonMap));
+    }
 
-        jsonSchema.read(new FileReader("src/test/resources/schemas/sample-product.json"));
+    @Test
+    public void testJsonTextWithNestedMap() {
+        Map<String, Object> nestedMap = new HashMap<>();
+        nestedMap.put("nestedKey1", "nestedValue1");
 
-        jsonSchema.jsonText(new HashMap<>());
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("key1", "value1");
+        jsonMap.put("key2", nestedMap);
 
+        String expectedJson = "{\"key1\":\"value1\",\"key2\":{\"nestedKey1\":\"nestedValue1\"}}";
+        assertEquals(expectedJson, jsonSchema.jsonText(jsonMap));
+    }
+
+    @Test
+    public void testJsonTextWithList() {
+        List<Object> list = Arrays.asList("item1", 123, true);
+
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("key1", list);
+
+        String expectedJson = "{\"key1\":[\"item1\",123,true]}";
+        assertEquals(expectedJson, jsonSchema.jsonText(jsonMap));
+    }
+
+    @Test
+    public void testJsonTextWithSpecialCharacters() {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("key1", "value \"1\"");
+        jsonMap.put("key2", "line\nbreak");
+
+        String expectedJson = "{\"key1\":\"value \\\"1\\\"\",\"key2\":\"line\\nbreak\"}";
+        assertEquals(expectedJson, jsonSchema.jsonText(jsonMap));
+    }
+
+    @Test
+    public void testReadMethodNotImplemented() {
+        Exception exception = assertThrows(UnsupportedOperationException.class, () -> {
+            jsonSchema.read(null);
+        });
+
+        String expectedMessage = "Not yet implemented";
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
