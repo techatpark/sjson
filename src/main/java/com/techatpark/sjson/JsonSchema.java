3f3afa7190c1f8f9cd9a010697f88a4dc775793a
hks-26 <himanshusingh020675@gmail.com>
2024-01-22 16:45:49 +0530

    changes updated

--- src/main/java/com/techatpark/sjson/JsonSchema.java
+++ src/main/java/com/techatpark/sjson/JsonSchema.java
@@ -1,183 +1,105 @@
 package com.techatpark.sjson;
 
-import com.techatpark.sjson.util.NumberParser;
-
-
-
-import java.io.IOException;
-import java.io.Reader;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 
 /**
- * Json Schema that represents the schema document.
- * Ref: https://json-schema.org/specification
+ * Represents a JSON schema document. Provides functionality to serialize
+ * Java objects to JSON strings.
  */
-public class JsonSchema {
+public final class JsonSchema {
 
     /**
-     * @param reader file reader for json schema
+     * Constructor for JsonSchema.
      */
-    public JsonSchema(final Reader reader) {
-
+    public JsonSchema() {
+        // Constructor logic if any
     }
 
     /**
-     * Reads JSON as a Java Object.
-     * <p>
-     * It will return native java objects as given below based
-     * on JSON Data Type.
-     * Ref: https://www.w3schools.com/js/js_json_datatypes.asp
-     * <p>
-     * string - java.lang.String
-     * number - java.lang.Number
-     * object - java.util.Map
-     * array  - java.util.List
-     * boolean - java.lang.Boolean
-     * null - null
+     * Reads JSON from a Reader and converts it into a Java Object.
+     * This method is not yet implemented.
      *
-     * @param reader - file reader for json data
-     * @return object
-     * @throws IOException - throws io exception
+     * @param reader the Reader to read JSON data from
+     * @return Object representation of the read JSON
+     * @throws IOException if an I/O error occurs
      */
     public Object read(final Reader reader) throws IOException {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     /**
-     * Get Json text for the Map.
+     * Converts a Map into its JSON string representation.
      *
-     * @param jsonMap
-     * @return jsonText
+     * @param jsonMap the Map representing the JSON object
+     * @return the JSON string representation of the Map
      */
     public String jsonText(final Map<String, Object> jsonMap) {
-        StringBuilder builder = new StringBuilder();
-        boolean isFirst = true;
-        builder.append("{");
-        for (Map.Entry<String, Object> entry : jsonMap.entrySet()) {
-            if (isFirst) {
-                isFirst = false;
-            } else {
-                builder.append(",");
-            }
-            // Create Key enclosed with "
-            builder.append("\"")
-                    .append(escapeJsonTxt(entry.getKey()))
-                    .append("\":"); // Create Key value separator
-
-            Object value = entry.getValue();
-
-            valueText(builder, value);
-        }
-        return builder.append("}").toString();
+        StringJoiner joiner = new StringJoiner(",", "{", "}");
+        jsonMap.forEach((final String key, final Object value) ->
+                joiner.add(formatEntry(key, value)));
+        return joiner.toString();
     }
 
     /**
-     * Create Value in according to the Type.
+     * Formats a key-value pair into a JSON entry.
      *
-     * @param builder
-     * @param value
+     * @param key   the key in the map
+     * @param value the value corresponding to the key
+     * @return formatted JSON entry
      */
-    private void valueText(final StringBuilder builder, final Object value) {
+    private String formatEntry(final String key, final Object value) {
+        return String.format("\"%s\":%s", escapeJson(key), valueToJson(value));
+    }
+
+    /**
+     * Converts a value to its JSON representation.
+     *
+     * @param value the value to convert
+     * @return JSON representation of the value
+     */
+    private String valueToJson(final Object value) {
         if (value == null) {
-            builder.append("null");
+            return "null";
         } else if (value instanceof String) {
-            processString(builder, (String) value);
+            return "\"" + escapeJson((String) value) + "\"";
         } else if (value instanceof Map) {
-            builder.append(jsonText((Map<String, Object>)
-                    value));
+            return jsonText((Map<String, Object>) value);
         } else if (value instanceof List) {
-            builder.append(jsonText((Map<String, Object>) value));
+            return listToJson((List<?>) value);
         } else {
-            builder.append(value);
+            return value.toString();
         }
     }
 
-
     /**
-     * Process String.
+     * Converts a list to its JSON array representation.
      *
-     * @param builder
-     * @param value
+     * @param list the list to convert
+     * @return JSON array representation of the list
      */
-    private void processString(final StringBuilder builder,
-                               final String value) {
-        builder.append("\"")
-                .append(escapeJsonTxt(value))
-                .append("\"");
+    private String listToJson(final List<?> list) {
+        StringJoiner sj = new StringJoiner(",", "[", "]");
+        list.forEach(obj -> sj.add(valueToJson(obj)));
+        return sj.toString();
     }
 
     /**
-     * Escape JSON Text.
-     * Escape quotes, \, /, \r, \n, \b, \f, \t
-     * and other control characters (U+0000 through U+001F).
-     * @param s
-     * @return escapeJsonTxt
+     * Escapes special characters in a JSON string.
+     *
+     * @param text the string to escape
+     * @return escaped JSON string
      */
-    private String escapeJsonTxt(final String s) {
-        if (s == null) {
-            return null;
-        }
-        StringBuilder sb = new StringBuilder();
-        escape(s, sb);
-        return sb.toString();
+    private String escapeJson(final String text) {
+        return text.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\b", "\\b")
+                .replace("\f", "\\f")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
     }
-
-    /**
-     * Escape Text.
-     * @param s - Must not be null.
-     * @param sb
-     */
-    private void escape(final String s, final StringBuilder sb) {
-        final int len = s.length();
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
-            switch (ch) {
-                case '"':
-                    sb.append("\\\"");
-                    break;
-                case '\\':
-                    sb.append("\\\\");
-                    break;
-                case '\b':
-                    sb.append("\\b");
-                    break;
-                case '\f':
-                    sb.append("\\f");
-                    break;
-                case '\n':
-                    sb.append("\\n");
-                    break;
-                case '\r':
-                    sb.append("\\r");
-                    break;
-                case '\t':
-                    sb.append("\\t");
-                    break;
-                case '/':
-                    sb.append("\\/");
-                    break;
-                default:
-                    //Reference: http://www.unicode.org/versions/Unicode5.1.0/
-                    if ((ch >= '\u0000' && ch <= '\u001F')
-                            || (ch >= '\u007F' && ch <= '\u009F')
-                            || (ch >= '\u2000' && ch <= '\u20FF')) {
-                        String ss = Integer.toHexString(ch);
-                        sb.append("\\u");
-                        for (int k = 0; k < NumberParser.NUMBER_FOUR
-                                - ss.length(); k++) {
-                            sb.append('0');
-                        }
-                        sb.append(ss.toUpperCase());
-                    } else {
-                        sb.append(ch);
-                    }
-            }
-        }
-    }
-
-
 }
---
+++ src/main/java/com/techatpark/sjson/generator/JsonSchemaGenerator.java
@@ -0,0 +1,116 @@
+/**
+ * Provides Json Generator
+ */
+package com.techatpark.sjson.generator;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * JsonSchemaGenerator is a utility class for
+ * generating JSON schemas from Java classes.
+ * It provides a method {@code create} that takes a Class object
+ * and returns a JSON schema
+ * representing the structure of the class.
+ */
+public class JsonSchemaGenerator {
+
+    /**
+     * Generates a JSON schema for the given Class.
+     *
+     * @param aClass The Class for which the JSON schema is to be generated.
+     * @return A JSON schema representing the structure of the given Class.
+     */
+
+//    This Json schema generator is made for the Java types
+
+    public String create(final Class<?> aClass) {
+        StringBuilder schemaBuilder =
+                new StringBuilder("{\"type\":\"object\",\"properties\":{");
+
+        Field[] fields = aClass.getDeclaredFields();
+        boolean isFirstField = true;
+
+        for (Field field : fields) {
+            if (!isFirstField) {
+                schemaBuilder.append(",");
+            } else {
+                isFirstField = false;
+            }
+
+            String fieldName = field.getName();
+            Type fieldType = field.getGenericType();
+
+            schemaBuilder.append("\"").append(fieldName).append("\":");
+
+            if (fieldType instanceof ParameterizedType) {
+                schemaBuilder.
+                        append(generateParameterizedTypeSchema(fieldType));
+            } else {
+                schemaBuilder.append("{\"type\":\"")
+                        .append(getJsonType(fieldType)).append("\"}");
+            }
+        }
+
+        schemaBuilder.append("}");
+        schemaBuilder
+                .append(",\"$schema\":\""
+                        + "http://json-schema.org/draft-04/schema#")
+                .append("\"}");
+        return schemaBuilder.toString();
+    }
+
+    /**
+     * Generates a JSON schema for the given parameterized type.
+     *
+     * @param fieldType The parameterized type for which
+     * the JSON schema is to be generated.
+     * @return A JSON schema representing the structure of
+     * the given parameterized type.
+     */
+    String generateParameterizedTypeSchema(final Type fieldType) {
+        if (fieldType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) fieldType;
+            Type[] typeArguments = parameterizedType.getActualTypeArguments();
+
+//            That is using for the type arguments
+
+            if (typeArguments.length == 1 && typeArguments[0] == String.class) {
+                return "{\"type\":\"array\",\"items\":{\"type\":\"string\"}}";
+            } else {
+                // Handle other parameterized types as needed...
+                return "{\"type\":\"array\",\"items\":{\"type\":\"unknown\"}}";
+            }
+        } else {
+            return "{\"type\":\"unknown\"}";
+        }
+    }
+
+    /**
+     * Determines the JSON type for the given field type.
+     *
+     * @param fieldType The Type of the field.
+     * @return The JSON type corresponding to the field type.
+     */
+//    This is for determining the java class
+    String getJsonType(final Type fieldType) {
+        if (fieldType == int.class || fieldType == long.class
+                || fieldType == Integer.class || fieldType == Long.class) {
+            return "integer";
+        } else if (fieldType == String.class) {
+            return "string";
+        } else if (fieldType == double.class || fieldType == float.class
+                || fieldType == Double.class || fieldType == Float.class) {
+            return "number";
+        } else if (fieldType == List.class) {
+            return "{\"type\":\"array\",\"items\":{\"type\":\"unknown\"}}";
+        } else if (fieldType.getTypeName().equals("java.math.BigDecimal")) {
+            return "number";
+        } else {
+            return "unknown";
+        }
+    }
+}
+
