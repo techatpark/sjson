20899ea5be780f1d0f132aa269ca5b78af6bce12
sathishk <sathishkumar.thiyagarajan@gmail.com>
2024-01-31 18:58:02 +0530

    Latest Fix 222

--- src/main/java/com/techatpark/sjson/schema/JsonSchema.java
+++ src/main/java/com/techatpark/sjson/schema/JsonSchema.java
@@ -2,10 +2,8 @@ package com.techatpark.sjson.schema;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
 
 import jakarta.validation.ConstraintViolation;
 
@@ -52,68 +50,7 @@ public final class JsonSchema {
      * @return the JSON string representation of the Map
      */
     public String jsonText(final Map<String, Object> jsonMap) {
-        StringJoiner joiner = new StringJoiner(",", "{", "}");
-        jsonMap.forEach((final String key, final Object value) ->
-                joiner.add(formatEntry(key, value)));
-        return joiner.toString();
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    /**
-     * Formats a key-value pair into a JSON entry.
-     *
-     * @param key   the key in the map
-     * @param value the value corresponding to the key
-     * @return formatted JSON entry
-     */
-    private String formatEntry(final String key, final Object value) {
-        return String.format("\"%s\":%s", escapeJson(key), valueToJson(value));
-    }
-
-    /**
-     * Converts a value to its JSON representation.
-     *
-     * @param value the value to convert
-     * @return JSON representation of the value
-     */
-    private String valueToJson(final Object value) {
-        if (value == null) {
-            return "null";
-        } else if (value instanceof String) {
-            return "\"" + escapeJson((String) value) + "\"";
-        } else if (value instanceof Map) {
-            return jsonText((Map<String, Object>) value);
-        } else if (value instanceof List) {
-            return listToJson((List<?>) value);
-        } else {
-            return value.toString();
-        }
-    }
-
-    /**
-     * Converts a list to its JSON array representation.
-     *
-     * @param list the list to convert
-     * @return JSON array representation of the list
-     */
-    private String listToJson(final List<?> list) {
-        StringJoiner sj = new StringJoiner(",", "[", "]");
-        list.forEach(obj -> sj.add(valueToJson(obj)));
-        return sj.toString();
-    }
-
-    /**
-     * Escapes special characters in a JSON string.
-     *
-     * @param text the string to escape
-     * @return escaped JSON string
-     */
-    private String escapeJson(final String text) {
-        return text.replace("\\", "\\\\")
-                .replace("\"", "\\\"")
-                .replace("\b", "\\b")
-                .replace("\f", "\\f")
-                .replace("\n", "\\n")
-                .replace("\r", "\\r")
-                .replace("\t", "\\t");
-    }
 }
