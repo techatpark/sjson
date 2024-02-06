8ea115a5f18795a24170db07eba10f73fe7eded8
Jaydeo Sahu <Jaydeo.Sahu@cognizant.com>
2024-02-06 17:15:10 +0530

    added cases for primitive type and arrays

--- src/main/java/com/techatpark/sjson/schema/generator/JsonSchemaGenerator.java
+++ src/main/java/com/techatpark/sjson/schema/generator/JsonSchemaGenerator.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 
 /**
@@ -46,6 +47,8 @@ public class JsonSchemaGenerator {
             if (fieldType instanceof ParameterizedType) {
                 schemaBuilder.
                         append(generateParameterizedTypeSchema(fieldType));
+            } else if (field.getType().isArray()) {
+                schemaBuilder.append(getJsonTypeForArrays(fieldType));
             } else {
                 schemaBuilder.append("{\"type\":\"")
                         .append(getJsonType(fieldType)).append("\"}");
@@ -64,7 +67,7 @@ public class JsonSchemaGenerator {
      * Generates a JSON schema for the given parameterized type.
      *
      * @param fieldType The parameterized type for which
-     * the JSON schema is to be generated.
+     *                  the JSON schema is to be generated.
      * @return A JSON schema representing the structure of
      * the given parameterized type.
      */
@@ -75,6 +78,8 @@ public class JsonSchemaGenerator {
 
             if (typeArguments.length == 1 && typeArguments[0] == String.class) {
                 return "{\"type\":\"array\",\"items\":{\"type\":\"string\"}}";
+            } else if (typeArguments.length == 2) {
+                return "{\"type\":\"object\"}}";
             } else {
                 // Handle other parameterized types as needed...
                 return "{\"type\":\"array\",\"items\":{\"type\":\"unknown\"}}";
@@ -103,6 +108,31 @@ public class JsonSchemaGenerator {
             return "{\"type\":\"array\",\"items\":{\"type\":\"unknown\"}}";
         } else if (fieldType == BigDecimal.class) {
             return "number";
+        } else if (fieldType == boolean.class || fieldType == Boolean.class) {
+            return "boolean";
+        } else {
+            return "unknown";
+        }
+    }
+    /**
+     * Determines the JSON type for primitive array the given field type.
+     *
+     * @param fieldType The Type of the field.
+     * @return The JSON type corresponding to the field type.
+     */
+    String getJsonTypeForArrays(final Type fieldType) {
+        if (fieldType == int[].class || fieldType == Integer[].class
+                || fieldType == long[].class || fieldType == Long[].class
+                || fieldType == BigInteger[].class) {
+            return "{\"type\":\"array\",\"items\":{\"type\":\"integer\"}}";
+        } else if (fieldType == String[].class || fieldType == char[].class
+                || fieldType == Character[].class) {
+            return "{\"type\":\"array\",\"items\":{\"type\":\"string\"}}";
+        } else if (fieldType == double[].class || fieldType == Double[].class
+                || fieldType == float[].class || fieldType == Float[].class) {
+            return "{\"type\":\"array\",\"items\":{\"type\":\"number\"}}";
+        } else if (fieldType == BigDecimal[].class) {
+            return "{\"type\":\"array\",\"items\":{\"type\":\"number\"}}";
         } else {
             return "unknown";
         }
